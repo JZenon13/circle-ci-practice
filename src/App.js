ae27cb2c279d2f45195a36f0cbdb45fdcbe5a320
Jesse Zenon <jz.the.dev@gmail.com>
2023-03-29 10:00:15 -0500

    some new stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Circle CI on branch1</div>;
+  return (
+    <div className="App">
+      Circle CI on branch1
+      <h1>Let's see if this works</h1>
+    </div>
+  );
 }
 
 export default App;
